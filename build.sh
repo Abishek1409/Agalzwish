da61e4ac72925bc644f123f2b4b6510031fc3b95
Abishek_1409 <abisheklive09@gmail.com>
2026-06-16 11:56:00 +0530

    Inline Firebase config via sed in build script

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-# Generates firebase-config.js from Vercel environment variables at build time
-cat > firebase-config.js <<EOF
-window.firebaseConfig = {
-  apiKey:            "$FIREBASE_API_KEY",
-  authDomain:        "$FIREBASE_AUTH_DOMAIN",
-  projectId:         "$FIREBASE_PROJECT_ID",
-  storageBucket:     "$FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "$FIREBASE_MESSAGING_SENDER_ID",
-  appId:             "$FIREBASE_APP_ID",
-  measurementId:     "$FIREBASE_MEASUREMENT_ID"
-};
-EOF
+# Replace the firebase-config.js script tag with inline config from env vars
+sed -i "s|<script src=\"firebase-config.js\"></script>|<script>window.firebaseConfig={apiKey:\"$FIREBASE_API_KEY\",authDomain:\"$FIREBASE_AUTH_DOMAIN\",projectId:\"$FIREBASE_PROJECT_ID\",storageBucket:\"$FIREBASE_STORAGE_BUCKET\",messagingSenderId:\"$FIREBASE_MESSAGING_SENDER_ID\",appId:\"$FIREBASE_APP_ID\",measurementId:\"$FIREBASE_MEASUREMENT_ID\"};</script>|" index.html
