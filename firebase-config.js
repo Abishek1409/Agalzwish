e24a24aabfdbe9461700320e29d7d434b27ba5d5
Abishek_1409 <abisheklive09@gmail.com>
2026-06-16 12:08:17 +0530

    Commit firebase-config.js directly, remove build script

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+window.firebaseConfig = {
+  apiKey:            "AIzaSyBSa-mGGAFDw_4hDn8kz7Zm5zeR7q4s92E",
+  authDomain:        "agalzwish.firebaseapp.com",
+  projectId:         "agalzwish",
+  storageBucket:     "agalzwish.firebasestorage.app",
+  messagingSenderId: "538329101416",
+  appId:             "1:538329101416:web:578ccc57206b8c9d66b7dd",
+  measurementId:     "G-18M24T8CB9"
+};
